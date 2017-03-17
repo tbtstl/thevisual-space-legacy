bbd19a3c39c2c315adf38b4de6e122bd7a129e6d
Tyson Battistella <tysonbattistella@gmail.com>
2017-03-17 11:36:24 -0700

    added apply force method to mover

--- scripts/p5/utils/Mover.js
+++ scripts/p5/utils/Mover.js
@@ -6,6 +6,7 @@ export default class Mover {
       location: new Vector(0,0),
       velocity: new Vector(0, 0),
       acceleration: new Vector(0, 0),
+      mass: 1,
       width: 800,
       height: 800,
       color: 175,
@@ -17,6 +18,7 @@ export default class Mover {
     this.location = attrs.location;
     this.velocity = attrs.velocity;
     this.acceleration = attrs.acceleration;
+    this.mass = attrs.mass;
     this.canvasWidth = attrs.width;
     this.canvasHeight = attrs.height;
     this.color = attrs.color;
@@ -29,20 +31,34 @@ export default class Mover {
   defaultUpdate() {
     this.velocity = this.velocity.add(this.acceleration);
     this.location = this.location.add(this.velocity);
-    this.velocity = this.velocity.limit(5);
 
-    if((this.location.x > this.canvasWidth) || (this.location.x < 0)){
+    if(this.location.x > this.canvasWidth){
+      this.location.x = this.canvasWidth;
+      this.velocity.x = this.velocity.x * -1;
+    } else if (this.location.x < 0){
+      this.location.x = 0;
       this.velocity.x = this.velocity.x * -1;
     }
 
-    if((this.location.y > this.canvasHeight) || (this.location.y < 0)){
+    if(this.location.y > this.canvasHeight){
+      this.location.y = this.canvasHeight;
+      this.velocity.y = this.velocity.y * -1;
+    } else if (this.location.y < 0){
+      this.location.y = 0;
       this.velocity.y = this.velocity.y * -1;
     }
+
+    this.acceleration = this.acceleration.multiply(0);
   }
 
   defaultDisplay(){
     this.p.stroke(this.stroke);
     this.p.fill(this.color);
-    this.p.ellipse(this.location.x, this.location.y, 48, 48);
+    this.p.ellipse(this.location.x, this.location.y, this.mass*10, this.mass*10);
+  }
+
+  applyForce(f){
+    let force = f.divide(this.mass);
+    this.acceleration = this.acceleration.add(force);
   }
 }
